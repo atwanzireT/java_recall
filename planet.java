104ec9cb92543cff57f18db96f16500dd1a71308
atwanzireT <tatwanzire@gmail.com>
2022-12-01 08:02:20 +0300

    recalling java

---
+++ planet.java
@@ -0,0 +1,8 @@
+class Planet{
+	// public static void main(String[] args){
+	// 	System.out.printIn("Panet loading ...");
+	// }
+	public static void main(String[] args) {
+		System.out.println("Panet loading ...");
+	}
+}

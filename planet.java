9be467da0439c3b45c3b3f08cd1f4ed732181aaf
atwanzireT <tatwanzire@gmail.com>
2022-12-03 09:39:32 +0300

    Java classes

--- planet.java
+++ planet.java
@@ -1,8 +1,19 @@
+class Person{
+	String name; int age; String gender;
+	
+	void walk() {
+		System.out.println(name + " walking ...");
+	}
+}
+
 class Planet{
 	// public static void main(String[] args){
 	// 	System.out.printIn("Panet loading ...");
 	// }
 	public static void main(String[] args) {
-		System.out.println("Panet loading ...");
+		Person john = new Person();
+		john.name = "Johnny";
+		john.walk();
+		System.out.println("Panet executed ...");
 	}
 }

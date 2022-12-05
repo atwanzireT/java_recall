ddd1e930a67cdc22750e0b54a8866416429aa0ed
atwanzireT <tatwanzire@gmail.com>
2022-12-05 20:01:47 +0300

    java linked lists

---
+++ basics/linkedlists/linkedlists.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+class Linkedlists {
+    public static void main(String[] args) {
+        LinkedList <String> C = new LinkedList <String>();
+
+        // adding items to the LinkedList
+        C.add("John");
+        C.add("James");
+        C.add("Annet");
+
+        System.out.println(C);
+
+        for(String s : C){
+            System.out.println(C);
+        }
+    }
+}

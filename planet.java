f070ce1248461eb84c966df42be18b65db188ee2
atwanzireT <tatwanzire@gmail.com>
2022-12-05 16:08:56 +0300

    Java

--- planet.java
+++ planet.java
@@ -1,3 +1,27 @@
+/**
+ * GETTERS AND SETTERS
+ * the setter method takes a parameter and assigns it to an attribute
+ * the getter method returns the value of the attribute
+ * */
+class Vehicle{
+	private String color;
+	
+//	constructor
+	public Vehicle() {
+		// TODO Auto-generated constructor stub
+		color = "Red";
+	}
+	
+//	getter
+	public String getColor() {
+		return color;
+	}
+	
+//	setter
+	public void setColor(String c) {
+		this.color = c;
+	}
+}
 class Person{
 	String name; int age; String gender;
 	
@@ -14,6 +38,9 @@ class Planet{
 		Person john = new Person();
 		john.name = "Johnny";
 		john.walk();
-		System.out.println("Panet executed ...");
+		Vehicle v1 = new Vehicle();
+		v1.setColor("Black");
+//		System.out.println(v1.getColor());
+		System.out.println("Panet executed ... " + v1.getColor());
 	}
 }

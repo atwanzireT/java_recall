f1c3e879310cb34a22014fc755bd87a157365283
atwanzireT <tatwanzire@gmail.com>
2022-12-06 21:46:43 +0300

    enums

--- basics/enums/enums.java
+++ basics/enums/enums.java
@@ -1,6 +1,30 @@
-public class Enums {
+enum Season {
     WINTER,
     SPRING,
     SUMMER,
-    FALL,
+    FALL
+}
+
+class Enums {
+    public static void main(String[] args) {
+        Season a = Season.FALL;
+
+        switch (a) {
+            case WINTER:
+                System.out.println("It is winter season.");
+                break;
+            case SPRING:
+                System.out.println("It is spring season.");
+                break;
+            case SUMMER:
+                System.out.println("It is summer season.");
+                break;
+            case FALL:
+                System.out.println("It is fall season.");
+                break;
+            default:
+                System.out.println("I can't tell the season");
+                break;
+        }
+    }
 }

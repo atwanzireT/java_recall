62dcdd9e0bd2ac1eba673f389f3b074039d9a03f
atwanzireT <tatwanzire@gmail.com>
2022-12-14 11:49:50 +0300

    jpa repository

--- spring_demo/src/main/java/com/example/spring_demo/SpringDemoApplication.java
+++ spring_demo/src/main/java/com/example/spring_demo/SpringDemoApplication.java
@@ -1,32 +1,12 @@
 package com.example.spring_demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner commandlinerunner(StudentRepository studentRepository) {
-		return args -> {
-			Student maria = new Student(
-					"Maria",
-					"Jones",
-					"maria.jones@gmail.com",
-					21);
-			// Student ahmed = new Student(
-			// "Ahmed",
-			// "Ali",
-			// "ahmed.ali@amigoscode.edu",
-			// 18
-			// );
-			studentRepository.save(maria);
-		};
 	}
 }

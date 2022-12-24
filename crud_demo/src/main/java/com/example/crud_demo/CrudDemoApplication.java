edf0b2d26fb4b1b811351ac16b32502d4e74e5c2
atwanzireT <tatwanzire@gmail.com>
2022-12-24 14:23:43 +0300

    SBT

---
+++ crud_demo/src/main/java/com/example/crud_demo/CrudDemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.crud_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoApplication.class, args);
+	}
+	@GetMapping("home")
+	public String home(){
+		return "Hello Home";
+	}
+
+}
